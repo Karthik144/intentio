e151916e01880e4acc68cc142cc142c47b5b6254
“Karthik144” <=>
2024-05-16 15:10:03 -0400

    Fixed blocked page loading bug

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -1,38 +1,32 @@
-chrome.tabs.onActivated.addListener((tab) => {
-    console.log(tab); 
+chrome.webNavigation.onCompleted.addListener(
+  (details) => {
+   
+    // Ensure we're dealing with top-level documents
+    if (details.frameId === 0) {
+      console.log(details);
 
-    chrome.tabs.get(tab.tabId, (currentTabData) => {
-
-        // Always says status is loading, pendingUrl is "chrome://newtab/", and title is "New Tab"
-        console.log("Current Tab Data:", currentTabData); // Why is this taking a long time to update?
-        // Only after a reload does it work 
-
-        if (currentTabData && currentTabData.url) {
-
-            // Retrieve the dictionary of sites from storage 
-            chrome.storage.local.get("sites", (data) => {
-
-                if (data.sites) {
-
-                    // Extract all the keys into an array 
-                    const savedSites = Object.keys(data.sites); 
-
-                    console.log("SAVED SITES:", savedSites); 
-
-                    // Check if the current tab's url matches any of the saved sites 
-                    if (savedSites.includes(currentTabData.url!)){
-
-                        console.log("INSIDE IF STATEMENT"); 
-
-                        chrome.tabs.update(tab.tabId, { url: chrome.runtime.getURL('blocked.html') });
-
-                    }
-                }
-            })
-
+      // Retrieve the dictionary of sites from storage
+      chrome.storage.local.get("sites", (data) => {
+        if (data.sites) {
             
-        }
-    })
-}); 
+          // Extract all the keys into an array
+          const savedSites = Object.keys(data.sites);
+          console.log("SAVED SITES:", savedSites);
 
-export {}
+          // Check if the current tab's url matches any of the saved sites
+          if (savedSites.includes(details.url)) {
+            console.log("INSIDE IF STATEMENT");
+
+            // Update the tab to display the blocked page
+            chrome.tabs.update(details.tabId, {
+              url: chrome.runtime.getURL("blocked.html"),
+            });
+          }
+        }
+      });
+    }
+  },
+  { url: [{ schemes: ["http", "https"] }] } // Filter to only consider HTTP/HTTPS URLs
+);
+
+export {};
